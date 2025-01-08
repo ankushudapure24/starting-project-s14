071ab5557c176a45fb6db088770a580a7fe53c32
ankushudapure24 <ankush.udapure@kaliba.in>
2025-01-08 15:35:26 +0530

    Final Commit
    After all the modification

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import Users from './components/Users';
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Arjun" },
+  { id: "u2", name: "Ananya" },
+  { id: "u3", name: "Rohit" },
+  { id: "u4", name: "Priya" },
+  { id: "u5", name: "Kabir" },
+  { id: "u6", name: "Aditi" },
+  { id: "u7", name: "Vikram" },
+  { id: "u8", name: "Sneha" },
+  { id: "u9", name: "Karan" },
+  { id: "u10", name: "Isha" },
+  { id: "u11", name: "Rahul" },
+  { id: "u12", name: "Pooja" },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  }
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
